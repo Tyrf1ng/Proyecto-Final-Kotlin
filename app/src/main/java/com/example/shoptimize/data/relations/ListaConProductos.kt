01eded6c4814a74161d74f047b460af7c8f50c0a
Tyrfing <iepteamdelaja@gmail.com>
2025-12-17 19:10:54 -0300

    ADD refactorización de la clase ListaConProductos: se corrigen tipos de datos y se implementan métodos para calcular totales y agregar productos

--- app/src/main/java/com/example/shoptimize/data/relations/ListaConProductos.kt
+++ app/src/main/java/com/example/shoptimize/data/relations/ListaConProductos.kt
@@ -1,15 +1,30 @@
 package com.example.shoptimize.data.relations
 
 import androidx.room.Embedded
+import androidx.room.Junction
 import androidx.room.Relation
-import com.example.shoptimize.data.entities.ListaDeCompras
+import com.example.shoptimize.data.ListaDeCompra
 import com.example.shoptimize.data.Producto
+import com.example.shoptimize.data.ListaProductoCrossRef
 
 data class ListaConProductos(
-    @Embedded val lista: ListaDeCompras,
+    @Embedded var lista: ListaDeCompra,
     @Relation(
         parentColumn = "id",
-        entityColumn = "lista_id"
+        entityColumn = "id",
+        associateBy = Junction(
+            value = ListaProductoCrossRef::class,
+            parentColumn = "listaId",
+            entityColumn = "productoId"
+        )
     )
     val productos: List<Producto>
-)
+) {
+    fun calculateTotal(): Int {
+        return lista.total
+    }
+    
+    fun addProduct(producto: Producto) {
+        calculateTotal()
+    }
+}
